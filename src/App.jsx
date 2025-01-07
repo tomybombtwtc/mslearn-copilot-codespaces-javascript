7f0631c9bedfedaa174b8175511c49a0dc7e08cd
Prajual R <87564540+PrajualR@users.noreply.github.com>
2025-01-07 09:37:30 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Prajual R",
+  title: "Software engineer",
+  email: "prajualramesh@gmail.com",
+  gitHub: "PrajualR",
+  instagram: "_prajual_",
+  linkedIn: "Prajual R",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
