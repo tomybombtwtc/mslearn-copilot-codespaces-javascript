f9ee9601e26e09ed37161ede07ef5a759961ef29
jancabala <66017111+jancabala@users.noreply.github.com>
2025-03-26 13:09:10 +0000

    Update personal information and add social icon animations

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,16 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Jan Carlo Viray",
+  title: "data scientist",
+  email: "carlo@viray.com",
+  gitHub: "karlhans",
+  instagram: "karlhans",
+  linkedIn: "karlhans",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "softmicrokarlhans",
+  youTube: "edocnirp",
+  website: "https://viray.com"
 };
 
 const primaryColor = "#4E567E";
